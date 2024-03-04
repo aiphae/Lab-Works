210377ad65bc63bcd8e3e7f9779c68defac0beae
aiphae <vl.diachenkoo@gmail.com>
2024-03-04 11:55:00 +0200

    uploaded lab1-2

--- lab1/lab1-2/Program.cs
+++ lab1/lab1-2/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.ReadKey();
+﻿using Newtonsoft.Json;
+
+var example = new Dictionary<int, int>();
+var random = new Random();
+
+// Рандомна генерація словника
+for (var i = 0; i < random.Next(50); i++)
+{
+    int randomKey;
+    do
+    {
+        randomKey = random.Next(0, 1000);
+    } while (example.ContainsKey(randomKey));
+    example.Add(randomKey, random.Next(0, 100));
+}
+
+// Найбільший та найменший ключ
+int maxKey = int.MinValue, minKey = int.MaxValue;
+foreach (var key in example.Keys)
+{
+    if (key > maxKey) maxKey = key;
+    if (key < minKey) minKey = key;
+}
+
+// Видалення елемента з найбільшим ключем, заміна значення найменшного ключа
+int tempValue = example[maxKey];
+example.Remove(maxKey);
+example[minKey] = tempValue;
+
+// Запис у JSON
+string json = JsonConvert.SerializeObject(example);
+string path = "../../../example.json";
+File.WriteAllText(path, json);
+Console.WriteLine(json);
+
+Console.ReadKey();
