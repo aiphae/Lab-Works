fc54444139f2f12a216d8e631712b9df9dfab16a
aiphae <vl.diachenkoo@gmail.com>
2024-03-04 12:08:46 +0200

    uploaded lab1-3 assignment

--- lab1/lab1-3/Program.cs
+++ lab1/lab1-3/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.ReadKey();
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+const int PRICE = 300;
+
+var prices = new Dictionary<string, int>
+{
+    {"Молоко", 43},
+    {"Хліб", 25},
+    {"Чай зелений", 60},
+    {"Ікра лососева", 399},
+    {"Вино", 339},
+    {"Кава зернова", 629},
+    {"Сир", 69},
+    {"Шоколад Milka", 38},
+    {"Яловичий стейк", 144},
+    {"Банани", 70}
+};
+
+var moreExpensive = prices.Where(p => p.Value > PRICE);
+var lessExpensive = prices.Where(p => p.Value <= PRICE);
+
+Console.WriteLine("Товари дорожчі за 300 грн:");
+foreach (var product in moreExpensive)
+{
+    Console.WriteLine($"{product.Key}: {product.Value}");
+}
+
+Console.WriteLine("\nТовари за 300 грн або дешевше:");
+foreach (var product in lessExpensive)
+{
+    Console.WriteLine($"{product.Key}: {product.Value}");
+}
+
+Console.ReadKey();
